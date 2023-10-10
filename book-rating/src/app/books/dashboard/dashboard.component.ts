610659a6a2eae2be5f5d84e63af532fb835e9270
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2023-10-10 16:12:17 +0200

    interface book (duck typing)

--- book-rating/src/app/books/dashboard/dashboard.component.ts
+++ book-rating/src/app/books/dashboard/dashboard.component.ts
@@ -1,15 +1,35 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { CommonModule, JsonPipe, UpperCasePipe } from '@angular/common';
+import { Book } from '../shared/book';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [CommonModule],
+  imports: [JsonPipe],
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent {
 
-  books = ['Angular', 'AngularJS', 'jQuery'];
+  books: Book[] = [
+    {
+      isbn: '000',
+      title: 'Angular',
+      description: 'Tolles Buch',
+      rating: 5
+    },
+    {
+      isbn: '111',
+      title: 'AngularJS',
+      description: 'Altes Buch',
+      rating: 3
+    },
+    {
+      isbn: '222',
+      title: 'jQuery',
+      description: 'Blödes Buch',
+      rating: 1
+    }
+  ]
 
 }
