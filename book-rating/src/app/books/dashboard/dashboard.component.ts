77e080d1d0ed1ec12463ad503cf069a8110b1571
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2023-10-24 14:07:26 +0200

    http

--- book-rating/src/app/books/dashboard/dashboard.component.ts
+++ book-rating/src/app/books/dashboard/dashboard.component.ts
@@ -5,6 +5,7 @@ import { BookComponent } from '../book/book.component';
 import { Book } from '../shared/book';
 import { BookRatingService } from '../shared/book-rating.service';
 import { BookCreateComponent } from '../book-create/book-create.component';
+import { BookStoreService } from '../shared/book-store.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -16,41 +17,23 @@ import { BookCreateComponent } from '../book-create/book-create.component';
 })
 export class DashboardComponent {
 
-  books: Book[] = [
-    {
-      isbn: '000',
-      title: 'Angular',
-      description: 'Tolles Buch',
-      rating: 5
-    },
-    {
-      isbn: '111',
-      title: 'AngularJS',
-      description: 'Altes Buch',
-      rating: 3
-    },
-    {
-      isbn: '222',
-      title: 'jQuery',
-      description: 'Blödes Buch',
-      rating: 1
-    }
-  ];
+  books: Book[] = [];
 
-  bs = inject(BookRatingService);
+  ratingService = inject(BookRatingService);
+  storeService = inject(BookStoreService);
 
   constructor() {
-    // setTimeout(() => this.books = [], 3000);
+    this.storeService.getAllBooks().subscribe(books => this.books = books);
   }
 
   doRateUp(book: Book): void {
-    const ratedBook = this.bs.rateUp(book);
+    const ratedBook = this.ratingService.rateUp(book);
     // const ratedBook = { ...book, rating: book.rating + 1 };
     this.updateAndSortList(ratedBook);
   }
 
   doRateDown(book: Book): void {
-    const ratedBook = this.bs.rateDown(book);
+    const ratedBook = this.ratingService.rateDown(book);
     this.updateAndSortList(ratedBook);
   }
 
---
+++ book-rating/src/app/books/shared/book-store.service.ts
@@ -0,0 +1,22 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+import { Book } from './book';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BookStoreService {
+
+  httpClient = inject(HttpClient);
+
+  getAllBooks(): Observable<Book[]> {
+    return this.httpClient.get<Book[]>('https://api.angular.schule/books');
+  }
+
+  getSingleBook(isbn: string): Observable<Book> {
+    return this.httpClient.get<Book>('https://api.angular.schule/books/' + isbn);
+  }
+}
