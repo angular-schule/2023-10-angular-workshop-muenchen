326ccbdcf1bc3ebc586cd2f7a7ddc9b32c1689e0
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2023-10-25 14:21:25 +0200

    rxjs: async pipe

--- rxjs-playground/src/app/exercises/unsubscribe/unsubscribe.component.ts
+++ rxjs-playground/src/app/exercises/unsubscribe/unsubscribe.component.ts
@@ -1,51 +1,16 @@
-import { Component, OnDestroy } from '@angular/core';
-import { Subject, ReplaySubject, timer, Subscription, takeWhile, takeUntil } from 'rxjs';
+import { AsyncPipe } from '@angular/common';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { timer } from 'rxjs';
+
 import { HistoryComponent } from '../../shared/history/history.component';
 
 @Component({
   templateUrl: './unsubscribe.component.html',
   standalone: true,
-  imports: [HistoryComponent]
+  imports: [HistoryComponent, AsyncPipe],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class UnsubscribeComponent implements OnDestroy {
+export class UnsubscribeComponent {
 
-  logStream$ = new ReplaySubject<string | number>();
-  destroy$ = new Subject();
-
-  /**
-   * Öffne die Browser-Console: Dort siehst Du den Output eines Observables, das jede Sekunde einen Wert generiert.
-   * Navigiere zurück auf die Startseite und beobachte die Console:
-   * Die Subscription läuft weiter. Wir haben einen klassischen Memory Leak erzeugt ...
-   *
-   * Sorge dafür, dass die Subscription in der Methode ngOnDestroy() beendet wird!
-   * Sie wird beim Buttonklick und beim Wegnavigieren ausgelöst.
-   *
-   * Es gibt noch weitere Wege, das Problem zu lösen ...
-   */
-  constructor() {
-    const interval$ = timer(0, 3000);
-
-    interval$.pipe(
-
-      /******************************/
-
-      takeUntil(this.destroy$)
-
-      /******************************/
-
-    ).subscribe({
-      next: e => this.log(e),
-      error: err => this.log('❌ ERROR: ' + err),
-      complete: () => this.log('✅ COMPLETE')
-    });
-  }
-
-  ngOnDestroy() {
-    this.destroy$.next(undefined);
-  }
-
-  log(msg: string | number) {
-    console.log(msg);
-    this.logStream$.next(msg);
-  }
+  interval$ = timer(0, 1000)
 }
