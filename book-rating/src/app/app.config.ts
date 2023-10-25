7f849113f48240f2b0f2a69f52a5a4effd7cdd2c
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2023-10-25 16:44:10 +0200

    ng add @ngrx/store ng add @ngrx/effects ng add @ngrx/store-devtools ng add @ngrx/schematics ng g feature books/store/book --entity=false --api --defaults

--- book-rating/src/app/app.config.ts
+++ book-rating/src/app/app.config.ts
@@ -1,19 +1,23 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, isDevMode } from '@angular/core';
 import { TitleStrategy, provideRouter, withDebugTracing } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
 import { CustomTitleStrategy } from './custom-title-strategy';
+import { provideStore } from '@ngrx/store';
+import { provideEffects } from '@ngrx/effects';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideRouter(routes,
-      ///withDebugTracing()
-    ),
-    provideHttpClient(), // NEU
+    provideRouter(routes),
+    provideHttpClient(),
     {
-      provide: TitleStrategy,
-      useClass: CustomTitleStrategy,
+        provide: TitleStrategy,
+        useClass: CustomTitleStrategy,
     },
-  ]
+    provideStore(),
+    provideEffects(),
+    provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() })
+]
 };
