9b98b0ed7900c9b7843b88c6cbbd767da54a4ee1
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2023-10-25 17:05:32 +0200

    ngrx: (feature)state

--- book-rating/src/app/app.config.ts
+++ book-rating/src/app/app.config.ts
@@ -4,9 +4,11 @@ import { TitleStrategy, provideRouter, withDebugTracing } from '@angular/router'
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
 import { CustomTitleStrategy } from './custom-title-strategy';
-import { provideStore } from '@ngrx/store';
+import { provideState, provideStore } from '@ngrx/store';
 import { provideEffects } from '@ngrx/effects';
 import { provideStoreDevtools } from '@ngrx/store-devtools';
+import { bookFeature } from './books/store/book.reducer';
+import { BookEffects } from './books/store/book.effects';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -18,6 +20,10 @@ export const appConfig: ApplicationConfig = {
     },
     provideStore(),
     provideEffects(),
-    provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() })
+    provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() }),
+
+    provideState(bookFeature),
+    provideEffects(BookEffects)
+
 ]
 };
